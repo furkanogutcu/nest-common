addae4e6b6ee038e2f0fad23e991f5c05061c5ae
furkanogutcu <furkanogutcu.dev@gmail.com>
2025-08-21 00:44:52 +0300

    refactor: migrate from @anatine/zod-nestjs to nestjs-zod

--- src/pipes/app-zod-validation.pipe.ts
+++ src/pipes/app-zod-validation.pipe.ts
@@ -1,12 +1,12 @@
-import { ZodDtoStatic, ZodValidationPipe } from '@anatine/zod-nestjs';
 import { ArgumentMetadata, Injectable } from '@nestjs/common';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 import { AppValidationException } from '../exceptions';
 
 @Injectable()
 export class AppZodValidationPipe extends ZodValidationPipe {
   transform(value: unknown, metadata: ArgumentMetadata) {
-    const zodSchema = (metadata?.metatype as ZodDtoStatic)?.zodSchema;
+    const zodSchema = (metadata?.metatype as any)?.schema;
 
     if (!zodSchema) return value;
 
